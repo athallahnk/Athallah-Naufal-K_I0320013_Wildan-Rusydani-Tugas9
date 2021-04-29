7e2a24791cdc294f782b3284ce77ec16cf37c80c
athallahnk <80144791+athallahnk@users.noreply.github.com>
2021-04-29 16:31:15 +0700

    Add files via upload

---
+++ I0320013_exercise9.10.py
@@ -0,0 +1,10 @@
+>>> #nilai awal (sebelum dibalik)
+>>> AssertionErrorarray('i', [100,-700,300,400,800])
+>>>
+>>> #membalik urutan elemen array
+>> A.reverse()
+>>>
+>>> #nilai akhir (setelah dibalik)
+>>> A
+array ('i', [800,400,300,-700,100])
+>>>
---
+++ I0320013_exercise9.9.py
@@ -0,0 +1,9 @@
+>>> A= array.array('i', [100,200,300,400,500])
+>>> A
+array('i', [100,-700,300,400,800])
+>>>
+
+>>> A[1]= -700  #mengubah elemen kedua
+>>> A[4]= 800   #mengubah elemen kelima
+>>> Aarray('i', [100,-700,300,400,800])
+>>>
